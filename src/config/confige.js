8575b6d4302906ffe2ab694411c097f6bf879194
DoanhTran03 <doanhtran2003@gmail.com>
2023-06-13 06:27:48 -0400

    Install Firebase

---
+++ src/config/confige.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAPxaTvZcjT-qFhBmjJpYrjf48hC0i995A",
+  authDomain: "book-inventory-b2046.firebaseapp.com",
+  projectId: "book-inventory-b2046",
+  storageBucket: "book-inventory-b2046.appspot.com",
+  messagingSenderId: "1031225059255",
+  appId: "1:1031225059255:web:ee91c70270089ead006a32",
+  measurementId: "G-YE8MSX088B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
