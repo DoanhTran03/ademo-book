f6ec6e55c8dd232d04da4a1a128f5db00a48b902
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-23 08:11:47 -0400

    Add login__header to Login component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,18 +7,18 @@ import { useEffect } from "react"
 const Login = () => {
   window.addEventListener('load',animate);
   return (
-    // <div className="login">
-    //   <video className="video" autoPlay loop muted>
-    //     <source src={video}/>
-    //   </video>
-    //   <LoginForm></LoginForm>
-    // </div>
+    <div className="login">
+      <video className="video" autoPlay loop muted>
+        <source src={video}/>
+      </video>
+      <LoginForm></LoginForm>
     <div className="login__header">
         <h1>Ademo Book.</h1>
         <p className="introduction">Mange your inventory for futher exploration</p>
         <div className="center-line"></div>
         <span className="icon"><TbBook/></span>
         <p className="slogan">Fast, Format and Forward.</p>
+    </div>
     </div>
   )
 }
