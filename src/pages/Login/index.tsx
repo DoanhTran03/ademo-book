2a6f5098efb32b256b5baa12d3f91922aa9877cc
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-23 05:38:50 -0400

    Build and add style for login__header in index.css\Login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,21 @@
 import video from "../../assets/pexels-cottonbro-studio-4860897-1366x720-25fps.mp4"
 import LoginForm from "./LoginForm"
+import {TbBook} from "react-icons/tb"
 import "./index.css"
 const Login = () => {
   return (
-    <div className="login">
-      <video className="video" autoPlay loop muted>
-        <source src={video}/>
-      </video>
-      <LoginForm></LoginForm>
+    // <div className="login">
+    //   <video className="video" autoPlay loop muted>
+    //     <source src={video}/>
+    //   </video>
+    //   <LoginForm></LoginForm>
+    // </div>
+    <div className="login__header">
+        <h1>Ademo Book.</h1>
+        <p>Mange your inventory for futher exploration</p>
+        <div className="center-line"></div>
+        <span className="icon"><TbBook/></span>
+        <p>Fast, Format and Forward.</p>
     </div>
   )
 }
