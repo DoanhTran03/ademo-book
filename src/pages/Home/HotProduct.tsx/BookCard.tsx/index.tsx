0d05fcd19f5b159b98c02ed5ab8b08e4f0906d30
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-24 06:50:39 -0400

    Add hotbooks static data for AutoSlider/HotProduct/Home component

---
+++ src/pages/Home/HotProduct.tsx/BookCard.tsx/index.tsx
@@ -0,0 +1,8 @@
+
+const BookCard = () => {
+  return (
+    <div className='bookCard'>This is book Card</div>
+  )
+}
+
+export default BookCard
---
+++ src/pages/Home/hotbooks.ts
@@ -0,0 +1,27 @@
+import bookstorycover from "../../assets/hotbooks/book-story.webp";
+import differentwintercover from "../../assets/hotbooks/different-winter.webp"
+import harrypottercover from "../../assets/hotbooks/harry-potter.webp"
+import mordernspacecover from "../../assets/hotbooks/morden-space.webp"
+
+const hotbooks = [
+  {
+    name: "book story",
+    author: "Jean Lumier",
+    cover: bookstorycover,
+  },
+  {
+    name: "different winter",
+    author: "Mia Jackson",
+    cover: differentwintercover,
+  },
+  {
+    name: "harry potter",
+    author: "J.K.Rowling",
+    cover: harrypottercover,
+  },
+  {
+    name: "mordern space",
+    author: "Francisco Spencer",
+    cover: mordernspacecover
+  }
+];
