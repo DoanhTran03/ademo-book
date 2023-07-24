2d3abd7543395a40fac1bffc81f4602da236556a
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-23 22:31:15 -0400

    Build navigation on mobile device

--- src/pages/Home/NavBar.tsx/index.tsx
+++ src/pages/Home/NavBar.tsx/index.tsx
@@ -2,14 +2,16 @@ import sections from "../sections";
 import "../index.css";
 import {FaBars} from "react-icons/fa"
 import {RxCross2} from "react-icons/rx"
+import { useState } from "react";
 const NavBar = () => {
-  return (  
+  const [isCollapsed, setIsCollapsed] = useState(true)
+  return (
     <>
-    <span className="cross-icon"><FaBars/></span>
-    <nav className={"nav-bar"}>
+    <span className="cross-icon" onClick={() => {setIsCollapsed(false)}}><FaBars/></span>
+    <nav className={isCollapsed ? "nav-bar nav-bar--collapsed":"nav-bar"}>
       <div className="nav-bar__heading">
         <h1>Ademo Book.</h1>
-        <span className="icon"><RxCross2/></span>
+        <span className="icon" onClick={() =>{setIsCollapsed(true)}}><RxCross2/></span>
       </div>
       <ul className="nav-bar__links">
         {sections.map((section, index) => (
