4ff7ed66ca2d4d10c39538dd930a8fdeb23ee56d
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-26 12:08:38 -0400

    Build button in BookCard/BookGrid component for deleting operation

--- src/pages/Inventory/BookGrid/BookCard.tsx/index.tsx
+++ src/pages/Inventory/BookGrid/BookCard.tsx/index.tsx
@@ -1,9 +1,10 @@
 import { Book, NewBook } from "../../../../hooks/useBook";
 
 interface Props {
+  onDelete: (id: string) => void;
   book: Book | NewBook;
 }
-const BookCard = ({book}: Props) => {
+const BookCard = ({book,onDelete}: Props) => {
   return (
     <article className={"bookCard"}>
       <img src={book.bookURL}></img>
@@ -12,6 +13,7 @@ const BookCard = ({book}: Props) => {
       <p className="bookCard__description">
         {book.description}
       </p>
+      <button className="bookCard__btn" onClick={() => onDelete(book.id)}>Delete Book</button>
     </article>
   );
 };
--- src/pages/Inventory/BookGrid/index.tsx
+++ src/pages/Inventory/BookGrid/index.tsx
@@ -4,9 +4,12 @@ import BookCard from './BookCard.tsx';
 const BookGrid = () => {
     const {books , handleDelete} = useBook();
     console.log(books);
+    const onDelete = (id: string) => {
+        handleDelete(id);
+    }
   return (
     <div className="bookGrid">
-        {books.map(book => <BookCard key={book.id} book={book}></BookCard>)}
+        {books.map(book => <BookCard onDelete={(id) => onDelete(id)} key={book.id} book={book}></BookCard>)}
     </div>
   )
 }
